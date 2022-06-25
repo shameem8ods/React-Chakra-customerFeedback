242cb52e10c8b74d8094c0fe7ae6cc7234183762
shameem <shameem@shameems-MacBook-Pro.local>
2022-06-25 22:55:49 +0530

    full hieght issue fixed for mobile view

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CardRight1 from './components/CardRight1';
 
 function App() {
   return (
-    <Flex className="App" backgroundColor={['linear(to-b, #fff, rgba(255,150,105,1))','gray.300']} height={['100%','100vh']} justifyContent={'center'} alignItems='center' >
+    <Flex className="App" backgroundColor={['linear(to-b, #fff, rgba(255,150,105,1))','gray.300']} height='100vh' justifyContent={'center'} alignItems='center' >
       <Box boxShadow='dark-lg' rounded='md' bg='white' backgroundColor={'white'} w={['100%','80%',]} h={['100%','90%']} blur='2px' filter='auto' bgGradient={['linear(to-b, #fff, rgba(255,150,105,1))','linear(to-r, #fff, rgba(255,150,105,1))']}>
         <Flex h='100%' direction={['column','column','row']}>
         <CradLeft/>
